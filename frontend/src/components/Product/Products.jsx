758746e634415bcfa691881cf1ae1a2880b217a4
aniket777984 <aniket777984@gmail.com>
2022-04-20 01:58:25 +0530

    Ratings bug fixed

--- frontend/src/components/Product/Products.jsx
+++ frontend/src/components/Product/Products.jsx
@@ -76,7 +76,7 @@ const Products = ({ match }) => {
               valueLabelDisplay="auto"
               aria-labelledby="range-slider"
               min={0}
-              max={25000}
+              max={250000}
             />
             
             <Typography>Categories</Typography>
